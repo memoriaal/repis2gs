0668d95fe67cfff9ae27d033ed04c76e1bed1cd2
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2023-02-06 15:17:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const {google} = require('googleapis')
 
 const app = express()
+const port = 8080
 
 app.get('/', (req, res) => {
+    console.log(`Served on port ${port}`)
     res.send('foo')
 })
 
-const port = 8080
 app.listen(port, (req, res) => {
     console.log(`Listening on port ${port}`)
 })
